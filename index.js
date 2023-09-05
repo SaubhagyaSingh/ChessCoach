f4ec100fc4e9bf66b408fb3fbba48ff36227eff5
Saubhagya Singh <76896543+SaubhagyaSingh@users.noreply.github.com>
2023-09-05 14:29:40 +0530

    2 versions created

--- index.js
+++ index.js
@@ -28,6 +28,58 @@ function firstPageAnim() {
     });
 }
 
+function updatepage() {
+  var elem = document.getElementById("firsth1");
+  elem.innerHTML = "CHESS";
+  elem = document.getElementById("secondh1");
+  elem.innerHTML = "COACH";
+  elem = document.getElementById("mydis");
+  elem.innerHTML = "Based in Pune";
+  elem = document.getElementById("myinfo");
+  elem.innerHTML = "WORK EXPERIENCE OF 4+ YEARS";
+  elem = document.getElementById("jobprofile");
+  elem.innerHTML =
+    "PREVIOUSLY WORKED AT ZUGZWANG ACADEMY <i class=ri-arrow-right-up-fill></i>";
+  elem.href = "https://www.zugzwang.in";
+  elem = document.getElementById("t1");
+  elem.innerHTML = "BEGINNER";
+  elem = document.getElementById("t2");
+  elem.innerHTML = "INTERMEDIATE";
+  elem = document.getElementById("aboutmexd");
+  elem.innerHTML = "ADVANCED";
+  elem = document.getElementById("infoaboutme");
+  elem.innerHTML =
+    "I am Saubhagya, a chess enthusiast and coach based in Pune, India. With a profound love for the game, I guide aspiring players through intricate strategies, openings, middlegames, and endgames. My personalized approach aims to nurture both skill and passion, creating lasting connections to the world of chess.";
+  elem = document.getElementById("mybtn");
+  elem.innerHTML = "LICHESS";
+  elem.href = "https://lichess.org/@/saubhagya";
+  elem = document.getElementById("btn");
+  elem.innerHTML = "CHESS.COM";
+  elem.href = "https://www.chess.com/member/saubhi65";
+  elem = document.getElementById("img1");
+  elem.src = "../assets/img/c1.png";
+  elem = document.getElementById("img2");
+  elem.src = "../assets/img/c2.png";
+  elem = document.getElementById("img3");
+  elem.src = "../assets/img/c3.png";
+}
+
+function checkpage() {
+  const value = sessionStorage.getItem("value");
+  if (value == 1) {
+    updatepage();
+  }
+}
+
+function fun() {
+  sessionStorage.setItem("value", 1);
+  window.location.href = "./main.html";
+}
+function fun2() {
+  sessionStorage.setItem("value", 2);
+  window.location.href = "./main.html";
+}
+
 function circleBend() {
   var xscale = 1;
   var yscale = 1;
